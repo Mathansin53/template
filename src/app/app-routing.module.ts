4f9e5722e3619f7d63b2d5e4458b5e63c8c7f286
routes <mathansin53@gmail.com>
2023-06-04 15:29:49 +0530

    ded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PagesComponent } from './pages/pages.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'pages',component:PagesComponent},
+  {path:'about',component:AboutComponent},
+  {path:'services',component:ServicesComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
